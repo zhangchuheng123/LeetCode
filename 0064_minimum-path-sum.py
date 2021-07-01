ea2d931ca0b0e51093f1de99e83b3e9194897ff0
Zhang Chuheng <zhangchuheng123@live.com>
2021-07-01 18:43:46 +0800

    update

--- 0064_minimum-path-sum.py
+++ 0064_minimum-path-sum.py
@@ -18,9 +18,24 @@
 # 1 <= m, n <= 200
 # 0 <= grid[i][j] <= 100
 
+MAXN = 200 * 200 * 100 + 1
+
 class Solution:
     def minPathSum(self, grid):
         m = len(grid)
         n = len(grid[0])
-        
-        minlen = 
+
+        minlen = [0] * n 
+        minlen[0] = grid[0][0]
+        for j in range(1, n):
+            minlen[j] = minlen[j-1] + grid[0][j]
+
+        for i in range(1, m):
+            for j in range(n):
+                minlen[j] = min(minlen[j-1] if j > 0 else MAXN, minlen[j]) + grid[i][j]
+                
+        return minlen[n-1]
+
+if __name__ == '__main__':
+    print(Solution().minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
+    print(Solution().minPathSum([[1,2,3],[4,5,6]]))
